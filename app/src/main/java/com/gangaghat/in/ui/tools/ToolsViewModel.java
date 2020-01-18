db57829468482efc68bd55541037877a980646a4
Rishi Raj <rishiraj619march@gmail.com>
2020-01-18 19:51:56 +0530

    Adding the app

---
+++ app/src/main/java/com/gangaghat/in/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package com.gangaghat.in.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
